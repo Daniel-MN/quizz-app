bc39c3586486ef46f01a93d9988f3f0de5601009
Musuroi Daniel <musuroidaniel01@gmail.com>
2025-07-05 12:37:50 +0300

    Kinesiologie improved

--- pdfsplitter.py
+++ pdfsplitter.py
@@ -2,15 +2,15 @@ from PyPDF2 import PdfReader, PdfWriter
 
 input_path = "LECTII LICENTA.pdf"  # Replace with your PDF filename
 reader = PdfReader(input_path)
-total_pages = 75
+total_pages = 29
 counter = 2
 
-for i in range(63, total_pages, 3):
+for i in range(4, total_pages, 29):
     writer = PdfWriter()
-    for j in range(i, min(i + 3, total_pages)):
+    for j in range(i, min(i + 29, total_pages)):
         writer.add_page(reader.pages[j])
     
-    output_filename = f"BGK/{counter}.pdf"
+    output_filename = f"Kinesiologie improved/{counter}.pdf"
     with open(output_filename, "wb") as output_pdf:
         writer.write(output_pdf)
     counter += 1
--- randomize_questions.py
+++ randomize_questions.py
@@ -1,8 +1,8 @@
 import random
 
 # Numele fișierului sursă și al celui de ieșire
-input_file = "chapter3_unshuffle.csv"
-output_file = "chapter3.csv"
+input_file = "chapter4_unshuffle.csv"
+output_file = "chapter4.csv"
 
 with open(input_file, "r", encoding="utf-8") as fin, open(output_file, "w", encoding="utf-8") as fout:
     for line in fin:
